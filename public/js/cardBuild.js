a96a4470e9c065d8e59106025542da62f5b18d85
John Farrell <johnfarrell@172-16-217-226.DYNAPOOL.NYU.EDU>
2016-04-25 17:18:30 -0400

    improving

--- public/js/cardBuild.js
+++ public/js/cardBuild.js
@@ -5,51 +5,17 @@ var Buttons = React.createClass({
 		};
 	},
 
-	handleClick: function(e) {
-		var park = e.currentTarget.getAttribute('id');
-		$.get('/info/'+park, function(data) {
+	handleClick: function() {
+		$.get('/info/'+this.props.name, function(data) {
 			console.log("res: " + data);
+			ReactDOM.render(<Card parkname={data}/>, document.getElementById('infoContainer'));
 		});
 	},
 
 	render: function() {
-
 		return (
 		<div>
-			<div className={"btn-group btn-group-justified"} role="group">
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="smokyMountains">Great Smoky Mountains</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="grandCanyon">Grand Canyon</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="rockyMountain">Rocky Mountain</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="yosemite">Yosemite</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="yellowstone">Yellowstone</button>
-				</div>
-			</div>
-			<div className={"btn-group btn-group-justified"} role="group">
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="zion">Zion</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="olympic">Olympic</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="grandTeton">Grand Teton</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="acadia">Acadia</button>
-				</div>
-				<div className={"btn-group"} role="group">
-			    	<button type="button" onClick={this.handleClick} className={"btn btn-default"} id="glacier">Glacier</button>
-				</div>
-			</div>
+			<button type="button" onClick={this.handleClick} className={"btn btn-default"} name={this.props.id}>{this.props.display}</button>			
 		</div>
 
 		)
@@ -74,7 +40,7 @@ var Card = React.createClass({
         			<h4 className={"card-title"} id="cardTitle">Facts</h4>
         			<ul className={"list-group list-group-flush"}>
 						<li className={"list-group-item"}>
-						<span className={"cardAttrs"} id="parkName">Name:</span>
+						<span className={"cardAttrs"} id="parkName">Name: {this.props.parkname}</span>
 						</li>
 					</ul>
         		</div>
@@ -83,5 +49,14 @@ var Card = React.createClass({
   	}
 });
  
-ReactDOM.render(<Card />, document.getElementById('infoContainer'));
-ReactDOM.render(<Buttons />, document.getElementById('buttonContainer'));
+
+ReactDOM.render(<Buttons name="smokyMountains" display="Great Smoky Mountains"/>,  document.getElementById('btn-one'));
+ReactDOM.render(<Buttons name="grandCanyon" display="Grand Canyon"/>,  document.getElementById('btn-two'));
+ReactDOM.render(<Buttons name="rockyMountain" display="Rocky Mountain"/>,  document.getElementById('btn-three'));
+ReactDOM.render(<Buttons name="yosemite" display="Yosemite"/>,  document.getElementById('btn-four'));
+ReactDOM.render(<Buttons name="yellowstone" display="Zion"/>,  document.getElementById('btn-five'));
+ReactDOM.render(<Buttons name="zion" display="Zion"/>,  document.getElementById('btn-six'));
+ReactDOM.render(<Buttons name="olympic" display="Olympic"/>,  document.getElementById('btn-seven'));
+ReactDOM.render(<Buttons name="grandTeton" display="Grand Teton"/>,  document.getElementById('btn-eight'));
+ReactDOM.render(<Buttons name="acadia" display="Acadia"/>,  document.getElementById('btn-nine'));
+ReactDOM.render(<Buttons name="glacier" display="Glacier"/>,  document.getElementById('btn-ten'));

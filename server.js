67ebd19cee4aa6b4d5063767d910a43ccb29c9a6
jefarrell <johnefarrell18@gmail.com>
2016-07-20 14:52:36 -0400

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("Server listening at http://%s:%s", host, port);
